4f0d4b432b69fb9b87f8b3b5e6b063584a6e8f9f
nick <nc363@njit.edu>
2022-05-11 14:32:57 -0400

    big push

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,10 +1,24 @@
+import os
 from flask import Blueprint, cli
 from flask_sqlalchemy import SQLAlchemy
+from app import config
 
 db = SQLAlchemy()
-
 database = Blueprint('database', __name__,)
+
 
 @database.cli.command('create')
 def init_db():
+    db.create_all()
+
+
+@database.before_app_first_request
+# Holds uploaded files
+def create_upload_folder():
+    root = config.Config.BASE_DIR
+    # Sets the name of the apps log folder to logs
+    uploadfolder = os.path.join(root,'..',config.Config.UPLOAD_FOLDER)
+    # Makes a directory if it doesn't exist
+    if not os.path.exists(uploadfolder):
+        os.mkdir(uploadfolder)
     db.create_all()
--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -3,11 +3,24 @@ from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
 from flask_login import UserMixin
+from sqlalchemy.orm import relationship
+
+
+class Transaction(db.Model):
+    tablename = 'transactions'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.String(300), nullable=True, unique=False)
+    transaction_type = db.Column(db.String(300), nullable=True, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = relationship("User", back_populates="Transaction")
+
+    def init(self, amount, transaction_type):
+        self.amount = amount
+        self.transaction_type = transaction_type
 
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(300), nullable=False, unique=True)
