67e854263b2f58090f9d5b737836987ac1cc1272
nick <nc363@njit.edu>
2022-05-11 20:55:59 -0400

    added simple_pages from past projects

---
+++ tests/simple_pages_test.py
@@ -0,0 +1,16 @@
+"""
+This tests the homepage and extra information pages on the nav bar
+"""
+
+
+def test_request_index(client):
+    """ This ensures that the Index page gets the correct HTML response of status code 200 """
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_request_page_not_found(client):
+    """This ensures that a page that doesn't exist gets
+    the correct HTML response of status code 404"""
+    response = client.get("/page99")
+    assert response.status_code == 404
